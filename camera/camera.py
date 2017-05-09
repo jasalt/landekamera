485745ccbe7175727a1b80690dda6deac6fb0e9e
Jarkko Saltiola <jarkko.saltiola@gmail.com>
2017-05-09 09:16:21 +0000

    Camera initialization. Rsync upload wip.

--- camera/camera.py
+++ camera/camera.py
@@ -1,55 +1,61 @@
-from sh import identify, fswebcam, v4l2-ctl
+from sh import identify, fswebcam, rsync
+import sh
 import os
 import schedule
 import time
 import datetime
 
+from config import rsync_target  # string eg "user@example.com:~/landekamera/server/data/jpg/"
+
 def initialize_camera():
-    v4l2-ctl("-d", "0",
+    v4l2ctl = sh.Command("v4l2-ctl")
+    v4l2ctl("-d", "0",
              "-c", "focus_auto=0",
              "-c", "focus_absolute=0",
              "-c", "white_balance_temperature_auto=0",
              "-c", "white_balance_temperature=6500")
-    # v4l2-ctl("-d", "0", "-c", "focus_absolute=0")
-    # v4l2-ctl("-d", "0", "-c", "white_balance_temperature_auto=0")
-    # v4l2-ctl("-d", "0", "-c", "white_balance_temperature=6500")
+    # v4l2ctl("-d", "0", "-c", "focus_absolute=0")
+    # v4l2ctl("-d", "0", "-c", "white_balance_temperature_auto=0")
+    # v4l2ctl("-d", "0", "-c", "white_balance_temperature=6500")
     print("Camera settings initialized")
 
 def calculate_deviation(image):
     deviation = str(identify('-format', ' \"%[standard-deviation]\"', image))
     return float(deviation[1:-1])  # strip quotes, convert to float
 
-def image_is_dark(image):
+def picture_is_dark(picture):
     min_deviation = 850  # pretty much black with c930e
-    deviation = calculate_deviation(image)
+    deviation = calculate_deviation(picture)
 
     if deviation < min_deviation:
-        print("Image too dark. Deviation: %s, Limit: %s." % (deviation, min_deviation))
+        print("Picture too dark. Deviation: %s, Limit: %s." % (deviation, min_deviation))
         return True
 
-    print("Image not too dark. Deviation: %s, Limit: %s." % (deviation, min_deviation))
+    print("Picture OK (not dark). Deviation: %s, Limit: %s." % (deviation, min_deviation))
     return False
 
 
-image_is_dark("out.jpg")
-# if dark.. wait long?
 
+# if dark.. sleep longer?
 # schedule every 30 seconds
 
 def take_picture():
     now = datetime.datetime.now()
-    timestamp = now.strftime("%y%m%d-%H%M%S")
-    dirname = "data/%s/" %timestamp
+    
+    date = now.strftime("%y%m%d")
+    dirname = "data/%s/" %date
+    
+    timestamp = date + "-" + now.strftime("%H%M%S")
+    filename = dirname + timestamp + ".jpg"    
 
     if not os.path.exists(dirname):
         os.makedirs(dirname)
 
-    filename = dirname + timestamp + ".jpg"    
-    # "data/day"
-
     # Try HDR?
     # exposure_auto (menu)   : min=0 max=3 default=0 value=3
     # exposure_absolute (int)    : min=3 max=2047 step=1 default=250 value=3 flags=inactive
+
+    print("Taking picture " + filename)
     
     fswebcam( "-S", "150",  # skip some frames for auto exposure 
               "--frames", "4",
@@ -58,30 +64,38 @@ def take_picture():
               "--no-banner",
               "--save", filename)
 
-    if image_is_dark(filename):
-        os.remove(filename)
+    if picture_is_dark(filename):
+        os.remove(filename)  # don't save pictures that don't have any light
         return
     
-    exiftool(filename, "-overwrite_original", "-GPSLatitudeRef=N", "-GPSLatitude=61.892220", "-GPSLongitudeRef=E", "-GPSLongitude=25.655319", "-GPSImgDirectionRef=T", "-GPSImgDirection=290", "-Model=\"Logitech C930e\"")
+#    exiftool(filename, "-overwrite_original", "-GPSLatitudeRef=N", "-GPSLatitude=61.892220", "-GPSLongitudeRef=E", "-GPSLongitude=25.655319", "-GPSImgDirectionRef=T", "-GPSImgDirection=290", "-Model=\"Logitech C930e\"")
+
+def process_log(line):
+    print(line)
 
 
 def upload_dir(dir):
-    # rsync
-    print("TODO uploading directory %s to server" % dir)
+    print("Uploading directory %s to server." % dir)
+    rsync("-avn", "--remove-source-files", "--ignore-existing", dir, rsync_target, _err=process_log, _out=process_log)
+
 
 def upload_previous_days():
     today = datetime.datetime.now().strftime("%y%m%d")
     dirs = os.listdir("data")
     for dir in dirs:
         if dir is not today:
+            # import pdb; pdb.set_trace()        
             upload_dir("data/" + dir)
-            
 
-schedule.every(30).seconds.do(take_picture)
-schedule.every().day.at("03:00").do(upload_previous_days)
-schedule.every
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+# initialize_camera()            
+# take_picture()
+# schedule.every(30).seconds.do(take_picture)
+
+upload_previous_days()
+
+# schedule.every().day.at("03:00").do(upload_previous_days)
+# while True:
+#     schedule.run_pending()
+#     time.sleep(1)
     
--- camera/install-deps.sh
+++ camera/install-deps.sh
@@ -1,4 +1,4 @@
-sudo apt-get -y install fswebcam curl python-pip python-dev imagemagick libimage-exiftool-perl
+sudo apt-get -y install fswebcam curl python-pip python-dev imagemagick libimage-exiftool-perl tmux \
+                        ffmpeg mediainfo
 
-sudo pip install sh schedule
-
+sudo pip install sh schedule ipython ipdb
