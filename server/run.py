1c47ece5e89d0f84ab5ecaaaaed1fedd9763357d
Jarkko Saltiola <jarkko.saltiola@gmail.com>
2017-06-01 19:07:49 +0300

    Print help, pass day arg to --instagram.
    Allows uploading a single day to Instagram.

--- server/run.py
+++ server/run.py
@@ -15,13 +15,15 @@ def run_threaded(job_func):
     job_thread = threading.Thread(target=job_func)
     job_thread.start()
 
-def upload_yesterday_ig():
-    today = datetime.datetime.now().strftime("%y%m%d")
-    yesterday = str(int(today) - 1)
+def upload_day_instagram(day=None):
+    if day is None:
+        today = datetime.datetime.now().strftime("%y%m%d")
+        yesterday = str(int(today) - 1)
+        day=yesterday
 
-    video = timelapse.jpgs_to_mp4("data/" + yesterday, "data/")
-    uploader.instagram(video, yesterday)
-    print("everything seems ok, should rm video now")
+    video = timelapse.jpgs_to_mp4("data/" + day, "data/")
+    uploader.instagram(video, day)
+    print("everything seems ok, TODO should rm video now")
     # If OK, remove video
 
 def upload_s3():
@@ -39,11 +41,12 @@ def upload_s3():
 
 def print_help():
     print('''Usage: python run.py
-                     --i             Run debug/interactive mode.
-                     --instagram     Starts daily Instagram uploader job.
-                     --camera        Starts taking timelapses.
-                     --youtube       TODO upload files to Youtube for archival, remove after uploaded?
-                     --loop          TODO delete oldest dir if less than 500MB space available on /
+  --i                      Run debug/interactive mode.
+  --instagram              Starts daily Instagram uploader job.
+  --instagram <timestamp>  Upload only a single timestamped video .
+  --camera                 Starts taking timelapses.
+  --youtube                TODO upload files to Youtube for archival, remove after uploaded?
+  --loop                   TODO delete oldest dir if less than 500MB space available on /
 
 Ideally you could run:
     python run.py --camera --instagram [TODO --youtube --loop]
@@ -54,15 +57,29 @@ But currently better to test every single functionality on it's own process/tmux
 if "--i" in sys.argv:
     print("Dropping to debugger/interactive mode.")
     import ipdb; ipdb.set_trace()
-    # upload_yesterday_ig()
+    # upload_day_instagram()
     # exit(1)
     
 if "--ig" in sys.argv:
+    
+    # take the only  command parameter that is not with -- as day param
+    
+    day_params = [x for x in sys.argv[-1:] if '--' not in x]
+    if day_params is not []:
+        day = day_params[-1:][0]
+        # import ipdb; ipdb.set_trace()
+        print("Uploading single \"%s\"day to Instagram." % day)
+        upload_day_instagram(day)
+        return
+
+    
+    import ipdb; ipdb.set_trace()
     print("Starting daily instagram upload job.")
-    schedule.every().day.at("03:00").do(upload_yesterday_ig)
+    schedule.every().day.at("03:00").do(upload_day_instagram)
+    
 
     # TODO run threaded
-    # schedule.every().day.at("03:00").do(run_threaded, upload_yesterday_ig)
+    # schedule.every().day.at("03:00").do(run_threaded, upload_day_instagram)
 
 
 if "--camera" in sys.argv:
